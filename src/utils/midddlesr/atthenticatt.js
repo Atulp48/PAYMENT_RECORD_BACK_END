8ecad865f9bc46845cf4ebafc813c71aa74b5d87
atul <patelatul948@gmail.com>
2024-01-13 12:09:07 +0530

    noe2

--- src/utils/midddlesr/atthenticatt.js
+++ src/utils/midddlesr/atthenticatt.js
@@ -1,3 +1,34 @@
+// import User from "../../models/User.js"
+// import Jwt from "jsonwebtoken";
+
+// export const authenticatt = async (req, res, next) => {
+
+
+//     try {
+//         const { token } = req.cookies;
+//         if (!token) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: "plase login first",
+//             })
+//         }
+
+//         Jwt.verify(token, process.env.JWT_S, (err, user) =>{
+//             if(err){
+//                 return res.status(500).json({success: false, message: err})
+//             }
+//             req.user = user;
+//         })
+//         next();
+
+//     }
+//     catch (error) {
+//         return res.status(500).json({ success: false, message: error.message })
+
+//     }
+// }
+
+
 import User from "../../models/User.js"
 import Jwt from "jsonwebtoken";
 
@@ -6,19 +37,16 @@ export const authenticatt = async (req, res, next) => {
 
     try {
         const { token } = req.cookies;
-        if (!token) {
-            return res.status(404).json({
-                success: false,
-                message: "plase login first",
-            })
-        }
 
-        Jwt.verify(token, process.env.JWT_S, (err, user) =>{
-            if(err){
-                return res.status(500).json({success: false, message: err})
-            }
-            req.user = user;
-        })
+        if (!token)
+          return res.status(404).json({
+            success: false,
+            message: "plase login first",
+          });
+      
+        const decoded = Jwt.verify(token, process.env.JWT_S);
+      
+        req.user = await User.findById(decoded._id);
         next();
 
     }
@@ -27,3 +55,8 @@ export const authenticatt = async (req, res, next) => {
 
     }
 }
+
+
+
+
+
